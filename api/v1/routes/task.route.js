4da428845238ce76a17d9d7a3f198b4e6e440ff4
son1228 <sonvipkl04@gmail.com>
2024-06-20 20:41:32 +0700

    sort by sortKey and sortValue

--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -2,16 +2,31 @@ const express = require('express')
 const router = express.Router()
 const Task = require('../../../models/task.model');
 
+
+// [GET] /api/v1/tasks
 router.get('/', async (req, res) => {
     try {
-        const tasks = await Task.find({});
+        const find = {
+            deleted: false,
+        }
+        if (req.query.status) {
+            find.status = req.query.status;
+        }
+        console.log(req.query);
+        // sort
+        const objSort = {}
+        if(req.query.sortKey && req.query.sortValue){
+            objSort[req.query.sortKey] = req.query.sortValue;
+        }
+        // obj sort
+        const tasks = await Task.find(find).sort(objSort);
         res.json(tasks);
     } catch (error) {
         res.send(error);
     }
 })
 
-
+// [GET] /api/v1/tasks/detail/:id
 router.get('/detail/:id', async (req, res) => {
     try {
         const tasks = await Task.findOne({
