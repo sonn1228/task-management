d39420ddf40e4238061a05ee8c3d4d742119565f
son1228 <sonvipkl04@gmail.com>
2024-06-24 17:28:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
 const bodyParser = require('body-parser')
 const app = express()
 require('dotenv').config();
+app.use(cors());
 
 const port = process.env.PORT;
 app.use(cors())
