6a4ed483b8881696654dac071624fe2bccddc210
son1228 <sonvipkl04@gmail.com>
2024-07-19 20:50:50 +0700

    [VALIDATE] Validate data

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -1,6 +1,7 @@
+const ForgotPassword = require('../models/forgot-password.model');
 const md5 = require('md5');
 const User = require('../models/user.model');
-const generate = require('../../../helpers/generate.helper');
+const generateHelper = require('../../../helpers/generate.helper');
 
 module.exports.register = async (req, res) => {
   try {
@@ -21,7 +22,7 @@ module.exports.register = async (req, res) => {
         fullName: req.body.fullName,
         email: req.body.email,
         password: req.body.password,
-        token: generate.generateRandomString(30)
+        token: generateHelper.generateRandomString(30)
       });
       await user.save();
       const token = user.token;
@@ -37,3 +38,69 @@ module.exports.register = async (req, res) => {
     res.json(error)
   }
 }
+// [POST]/api/v1/users/login
+module.exports.login = async (req, res) => {
+  const existEmail = await User.findOne({
+    email: req.body.email,
+    deleted: false
+  })
+
+  if (!existEmail) {
+    res.json({
+      code: 400,
+      message: "Email not exist"
+    })
+    return;
+  }
+  const password = md5(req.body.password);
+  if (password != existEmail.password) {
+    res.json({
+      code: 400,
+      message: "Password not exist"
+    })
+    return;
+  }
+  const token = existEmail.token;
+  res.cookie('token', token);
+
+
+  res.json({
+    code: 200,
+    message: 'success',
+    token: token
+  })
+}
+
+module.exports.forgotPassword = async (req, res) => {
+  const email = req.body.email;
+  const user = await User.findOne({
+    email: email,
+    deleted: false
+  })
+  if (!user) {
+    res.json({
+      code: 400,
+      message: "Email not found!"
+    })
+    return;
+  }
+  const otp = generateHelper.generateRandomNumber(6);
+
+  const timeExpire = 5;
+
+  const objForgotPassword = {
+    email: email,
+    otp: otp,
+    expireAt: Date.now() + timeExpire * 60  // 5 phút
+  }
+  const forgotPassword = new ForgotPassword(objForgotPassword)
+  await forgotPassword.save();
+
+  res.json({
+    code: 200,
+  })
+}
+
+
+
+
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+const cors = require('cors')
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 require('dotenv').config()
-const cookieParser = require('cookie-parser')
+// const cookieParser = require('cookie-parser')
 
-app.use(cookieParser());
-
+// app.use(cookieParser());
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
